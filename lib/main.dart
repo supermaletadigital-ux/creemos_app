2633a877d1667a660cef6d92229d2e217c3d7b3f
supermaletadigital-ux <super.maleta.digital@gmail.com>
2026-02-08 15:27:36 -0400

    Create lib/main.dart file with Firebase initialization

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Creemos Santa Cruz'),
+        ),
+        body: Center(
+          child: Text('Welcome to Creemos Santa Cruz!'),
+        ),
+      ),
+    );
+  }
+}
